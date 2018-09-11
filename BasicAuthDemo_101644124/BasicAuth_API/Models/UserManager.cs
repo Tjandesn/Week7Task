7e2a9eb5f60856f557a63f0f8a3089326125332e
TJ Anderson <101644124@student.swin.edu.au>
2018-09-11 11:51:19 +1000

    Project completed, awaiting postman tests

---
+++ BasicAuthDemo_101644124/BasicAuth_API/Models/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace BasicAuth_API.Models
+{
+    public class UserManager
+    {
+        private BasicAuth_DBEntities _Context;
+
+        public UserManager()
+        {
+            _Context = new BasicAuth_DBEntities();
+        }
+
+        public bool ValidateUser(string userName, string password)
+        {
+            var result = _Context.UserMasters.SingleOrDefault(x => x.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase)
+            && x.Password == password);
+
+            return result != null ? true : false;
+        }
+    }
+}
---
+++ BasicAuthDemo_101644124/BasicAuth_DB/Tables/UserMaster.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UserMaster]
+(
+	[Id] int not null primary key,
+	[UserName] nvarchar(50) not null,
+	[Password] nvarchar(50) not null,
+	[IsActive] int not null
+)
